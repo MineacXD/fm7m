4423b8ac36b7d62621d5fc20bb539cf95d076d0e
MineacXD <loscaquis2@gmail.com>
2026-05-10 15:25:48 -0400

    created bullet 2 and 3 for the final fight

---
+++ bullet_nagito_2.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speedX = 1000
+var speedY = 0
+var angleBullet = 0
+	
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2(speedX, speedY).rotated(deg_to_rad(angleBullet))
+	rotation = velocity.angle()
+		
+	move_and_slide()
+	
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		Global.lives = Global.lives -1
+		#there used to be a returntopreviousscreen call here
+		#but you should put it on the main battle scene may
+
+func _on_screen_exited() -> void:
+	queue_free()
